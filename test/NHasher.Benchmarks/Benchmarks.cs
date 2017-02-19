1d4ab8992be7bd3920d14cd8098dcb2f0bff133c
Denis Kulikov <cdbiznes@gmail.com>
2017-02-19 23:01:10 +0300

    Add missing file with benchmarks

--- test/NHasher.Benchmarks/Benchmarks.cs
+++ test/NHasher.Benchmarks/Benchmarks.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Jobs;
-using BenchmarkDotNet.Order;
 
 namespace NHasher.Benchmarks
 {
@@ -9,35 +8,29 @@ namespace NHasher.Benchmarks
     [MemoryDiagnoser]
     public class Benchmarks
     {
-        private const int N = 10000;
-        private readonly byte[] _data;
+        private byte[] _data;
 
         private readonly MurmurHash3X64L128 _murmurHash3X64L128 = new MurmurHash3X64L128();
         private readonly XXHash32 _xxHash32 = new XXHash32();
         private readonly XXHash64 _xxHash64 = new XXHash64();
 
-        public Benchmarks()
+        [Params(4, 11, 25, 100, 1000, 10000)]
+        public int PayloadLength { get; set; }
+
+        [Setup]
+        public void SetupData()
         {
-            _data = new byte[N];
+            _data = new byte[PayloadLength];
             new Random(42).NextBytes(_data);
         }
 
         [Benchmark]
-        public byte[] MurmurHash3X64L128()
-        {
-            return _murmurHash3X64L128.ComputeHash(_data);
-        }
+        public byte[] MurmurHash3X64L128() => _murmurHash3X64L128.ComputeHash(_data);
 
         [Benchmark]
-        public byte[] XXHash32()
-        {
-            return _xxHash32.ComputeHash(_data);
-        }
+        public byte[] XXHash32() => _xxHash32.ComputeHash(_data);
 
         [Benchmark]
-        public byte[] XXHash64()
-        {
-            return _xxHash64.ComputeHash(_data);
-        }
+        public byte[] XXHash64() => _xxHash64.ComputeHash(_data);
     }
 }
