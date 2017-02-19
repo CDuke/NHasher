0e3aafb365d454ca465372d85261fa2447190b8c
Denis Kulikov <cdbiznes@gmail.com>
2017-02-19 23:00:26 +0300

    Add benchmark project

---
+++ test/NHasher.Benchmarks/Benchmarks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Order;
+
+namespace NHasher.Benchmarks
+{
+    [ClrJob, CoreJob]
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private const int N = 10000;
+        private readonly byte[] _data;
+
+        private readonly MurmurHash3X64L128 _murmurHash3X64L128 = new MurmurHash3X64L128();
+        private readonly XXHash32 _xxHash32 = new XXHash32();
+        private readonly XXHash64 _xxHash64 = new XXHash64();
+
+        public Benchmarks()
+        {
+            _data = new byte[N];
+            new Random(42).NextBytes(_data);
+        }
+
+        [Benchmark]
+        public byte[] MurmurHash3X64L128()
+        {
+            return _murmurHash3X64L128.ComputeHash(_data);
+        }
+
+        [Benchmark]
+        public byte[] XXHash32()
+        {
+            return _xxHash32.ComputeHash(_data);
+        }
+
+        [Benchmark]
+        public byte[] XXHash64()
+        {
+            return _xxHash64.ComputeHash(_data);
+        }
+    }
+}
---
+++ test/NHasher.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace NHasher.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
