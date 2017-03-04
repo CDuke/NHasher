d9c3dfb2953cae5150e01a175e4311296b45c166
Denis Kulikov <cdbiznes@gmail.com>
2017-03-05 01:48:49 +0300

    Add murmurhash and adler32 algoritm to benchmark

--- test/NHasher.Benchmarks/Benchmarks.cs
+++ test/NHasher.Benchmarks/Benchmarks.cs
@@ -11,8 +11,11 @@ namespace NHasher.Benchmarks
         private byte[] _data;
 
         private readonly MurmurHash128X64V3 _murmurHash128X64V3 = new MurmurHash128X64V3();
+        private readonly MurmurHash128X32V3 _murmurHash128X32V3 = new MurmurHash128X32V3();
+        private readonly MurmurHash32X32V3 _murmurHash32X32V3 = new MurmurHash32X32V3();
         private readonly XXHash32 _xxHash32 = new XXHash32();
         private readonly XXHash64 _xxHash64 = new XXHash64();
+        private readonly Adler32 _adler32 = new Adler32();
 
         [Params(4, 11, 25, 100, 1000, 10000)]
         public int PayloadLength { get; set; }
@@ -25,12 +28,21 @@ namespace NHasher.Benchmarks
         }
 
         [Benchmark]
-        public byte[] MurmurHash3X64L128() => _murmurHash128X64V3.ComputeHash(_data);
+        public byte[] MurmurHash128X64V3() => _murmurHash128X64V3.ComputeHash(_data);
+
+        [Benchmark]
+        public byte[] MurmurHash128X32V3() => _murmurHash128X32V3.ComputeHash(_data);
+
+        [Benchmark]
+        public byte[] MurmurHash32X32V3() => _murmurHash32X32V3.ComputeHash(_data);
 
         [Benchmark]
         public byte[] XXHash32() => _xxHash32.ComputeHash(_data);
 
         [Benchmark]
         public byte[] XXHash64() => _xxHash64.ComputeHash(_data);
+
+        [Benchmark]
+        public byte[] Adler32() => _adler32.ComputeHash(_data);
     }
 }
