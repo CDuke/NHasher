7d4aa480ef561a4fa5e8f8986c7c1581bbc183e5
Denis Kulikov <cdbiznes@gmail.com>
2017-03-05 20:23:34 +0300

    Fnv64A implementation added and tests

--- test/NHasher.Tests/Fnv64ATests.cs
+++ test/NHasher.Tests/Fnv64ATests.cs
@@ -3,17 +3,17 @@ using Xunit;
 
 namespace NHasher.Tests
 {
-    public class Fnv64Tests
+    public class Fnv64ATests
     {
         [Theory]
         [InlineData(0xcbf29ce484222325, "")]
-        [InlineData(0xaf63bd4c8601b7be, "a")]
-        [InlineData(0xaf63bd4c8601b7bd, "b")]
-        [InlineData(0x340d8765a4dda9c2, "foobar")]
-        [InlineData(0x6519bd6389aaa166, "Hello, world!")]
+        [InlineData(0xaf63dc4c8601ec8c, "a")]
+        [InlineData(0xaf63df4c8601f1a5, "b")]
+        [InlineData(0x85944171f73967e8, "foobar")]
+        [InlineData(0x38d1334144987bf4, "Hello, world!")]
         public void CheckHashes(ulong expectedHash, string input)
         {
-            using (var hasher = new Fnv64())
+            using (var hasher = new Fnv64A())
             {
                 var buffer = StringToBytes(input);
                 var hash = hasher.ComputeHashAsUInt64(buffer);
@@ -24,7 +24,7 @@ namespace NHasher.Tests
         [Fact]
         public void CheckHashSize()
         {
-            using (var hash = new Fnv64())
+            using (var hash = new Fnv64A())
             {
                 Assert.Equal(64, hash.HashSize);
             }
